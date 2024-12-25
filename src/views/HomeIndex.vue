a392a4d57431723dba1fd725cf214cbbe10cb1f6
方姣姣 <1275841634@qq.com>
2024-12-25 19:44:45 +0800

    chore：替换banner

--- src/views/HomeIndex.vue
+++ src/views/HomeIndex.vue
@@ -2,7 +2,7 @@
   <div class="common-layout">
     <el-carousel direction="horizontal" indicator-position="" height="650px">
       <el-carousel-item v-for="item in carouselList" :key="item">
-        <img src="@/assets/img/Banner.jpg" />
+        <img :src="imgTransfer(item)" />
       </el-carousel-item>
     </el-carousel>
     <div class="layout-tem">
@@ -20,11 +20,7 @@
 </template>
 <script setup>
 import { RouterLink } from "vue-router";
-const carouselList = [
-  "@/assets/img/Banner.jpg",
-  "@/assets/img/Banner.jpg",
-  "@/assets/img/Banner.jpg",
-];
+const carouselList = ["1", "2", "3", "4"];
 const list = [
   {
     name: "关于我们",
@@ -51,6 +47,9 @@ const list = [
     link: "/download",
   },
 ];
+const imgTransfer = (name) => {
+  return `src/assets/img/banner${name}.png`;
+};
 </script>
 <style lang="scss" scoped>
 .common-layout {
